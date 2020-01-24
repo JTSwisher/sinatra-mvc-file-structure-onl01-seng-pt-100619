3093e3c59de91981282aad3f0448cf9ee7e34d99
Jeff Swisher <jefftswisher@gmail.com>
2020-01-24 03:23:51 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,12 +4,11 @@ attr_accessor :name, :breed, :age
 
 @@all = []
 
-def initialize(name, breed, dog)
+def initialize(name, breed, age)
   @name = name 
   @breed = breed 
   @age = age 
   @@all << self 
-  
 end 
 
 def self.all 
